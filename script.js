bd0a07e18c104eb006821be1edf883eadd53a95f
Anna Svetlova <svetloovaanna@gmail.com>
2022-02-24 00:30:19 +0300

    add task 4

--- script.js
+++ script.js
@@ -1,12 +1,33 @@
-// Сохранение первой заметки
+let list = document.querySelector('.list');
+let addBtn = document.querySelector(".addBtn");
+let clearBtn = document.querySelector('.clearBtn');
+let noteInput = document.querySelector('.note');
+let itemsArray = [];
 
-note.value = localStorage.getItem('note');
-note.oninput = () => {
-localStorage.setItem('note', note.value)
-};
 
-function checkValidity() {
-    if(note.validity.valueMissing) {
-        alert('А где твоя заметка?');
-    }
+function addNote() {
+    let note = noteInput.value;
+
+    if (note.length === 0) return;
+
+    itemsArray.push(note);
+
+    localStorage.setItem('notes', itemsArray);
+
+    showNote(note);
+
+    noteInput.value = "";
 }
+
+function showNote(note) {
+    document.querySelector('.list').innerHTML = note;
+}
+
+function clearNotes() {
+    list.innerHTML = "";
+    localStorage.removeItem('notes');
+    itemsArray = [];
+}
+
+addBtn.addEventListener("click", addNote);
+clearBtn.addEventListener("click", clearNotes);
